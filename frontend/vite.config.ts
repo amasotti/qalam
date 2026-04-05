e05b878bd001016454870dd0443516b9fe314820
Antonio Masotti <toniomasotti@gmail.com>
2026-04-05 12:10:18 +0200

    feat: add local proxy for backend via vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,11 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		proxy: {
+			'/health': 'http://localhost:8080',
+			'/api':    'http://localhost:8080',
+		}
+	}
 });

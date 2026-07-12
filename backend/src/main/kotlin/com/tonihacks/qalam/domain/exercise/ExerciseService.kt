6ae92687829a477c2f060916f8be6e20186529b5
Antonio Masotti <toniomasotti@gmail.com>
2026-07-12 09:58:03 +0200

    test: fix imports

--- backend/src/main/kotlin/com/tonihacks/qalam/domain/exercise/ExerciseService.kt
+++ backend/src/main/kotlin/com/tonihacks/qalam/domain/exercise/ExerciseService.kt
@@ -3,7 +3,6 @@ package com.tonihacks.qalam.domain.exercise
 import arrow.core.Either
 import arrow.core.raise.either
 import arrow.core.raise.ensure
-import arrow.core.raise.raise
 import com.tonihacks.qalam.domain.error.DomainError
 import com.tonihacks.qalam.domain.logDomainFailure
 import com.tonihacks.qalam.domain.training.MasteryPromotionResponse
--- backend/src/test/kotlin/com/tonihacks/qalam/delivery/ExerciseIntegrationTest.kt
+++ backend/src/test/kotlin/com/tonihacks/qalam/delivery/ExerciseIntegrationTest.kt
@@ -4,13 +4,13 @@ import com.tonihacks.qalam.BaseIntegrationTest
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
 import io.ktor.client.HttpClient
-import io.ktor.client.request.contentType
 import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentType
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonObject
